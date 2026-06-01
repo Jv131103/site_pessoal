0971f5cf86f154ceafdf2f6852a0b1092490b33c
Jv131103 <jjv1792@gmail.com>
2026-06-01 16:12:58 -0300

    feat/site-pessoal

--- app.py
+++ app.py
@@ -111,4 +111,5 @@ if __name__ == '__main__':
     from waitress import serve
 
     # http://localhost:5000/
-    serve(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    serve(app, host="0.0.0.0", port=port)
